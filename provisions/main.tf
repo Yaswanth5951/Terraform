ef6777dec59b8d1c6b84a7c05679108520fdb57f
Yaswanth5951 <kudumulayaswanth1@gmail.com>
2024-01-06 17:59:57 +0530

    new changes

--- provisions/main.tf
+++ provisions/main.tf
@@ -8,13 +8,5 @@ resource "aws_instance" "example" {
   ami           = "ami-008fe2fc65df48dac"
   instance_type = "t2.micro"
   subnet_id = "subnet-0d6e3655d55ad86c3"
-  key_name = "Devops"
-
-  provisioner "remote-exec" {
-    inline = [
-       "sudo apt-get update",
-      "sudo apt-get install -y nginx"
-    ]
-  }
-            
+  key_name = "Devops"   
 }
